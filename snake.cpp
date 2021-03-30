efc50fa4f58bc044c4ba98b86fdba7b107e8d333
jarod <jarodiking17@gmail.com>
2021-03-30 13:59:43 +0200

    version 1.0.3
    tried to change map size, stopped doing this. Increasing map will cause more flickering

--- snake.cpp
+++ snake.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 bool gameOver;
 
-const int width = 40;
-const int height = 40;
+const int width = 20;
+const int height = 20;
 
 int x, y, fruitX, fruitY, score;
 int tailX[100], tailY[100]; // snail coordinates
